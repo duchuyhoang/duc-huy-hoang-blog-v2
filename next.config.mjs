52fc59a15c39993275f82803f4824d1928c8b4ad
Huy Hoang <huyhd2@vmogroup.com>
2024-08-15 15:48:47 +0700

    fix: fix build prod

--- next.config.mjs
+++ next.config.mjs
@@ -2,66 +2,6 @@
 import createMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  reactStrictMode: true,
-  transpilePackages: ["react-syntax-highlighter"],
-  output: "export",
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  distDir: "dist",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgoConfig: {
-                multipass: true,
-                plugins: [
-                  {
-                    name: "preset-default",
-                    params: {
-                      overrides: {
-                        cleanupIds: false,
-                        removeViewBox: false,
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-          },
-        ],
-      }
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
-    return config;
-  },
-  basePath: "/duc-huy-hoang-blog-v2",
-  assetPrefix: "/duc-huy-hoang-blog-v2",
-};
-
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
@@ -70,4 +10,70 @@ const withMDX = createMDX({
   },
 });
 
-export default withMDX(nextConfig);
+const createConfig = (phase, ...params) => {
+  const basePath = process.env.NEXT_PUBLIC_BASE_HREF;
+  return withMDX({
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    reactStrictMode: true,
+    transpilePackages: ["react-syntax-highlighter"],
+    output: "export",
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+    distDir: "dist",
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+    webpack(config) {
+      const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.(".svg")
+      );
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                svgoConfig: {
+                  multipass: true,
+                  plugins: [
+                    {
+                      name: "preset-default",
+                      params: {
+                        overrides: {
+                          cleanupIds: false,
+                          removeViewBox: false,
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            },
+          ],
+        }
+      );
+      fileLoaderRule.exclude = /\.svg$/i;
+      return config;
+    },
+    ...(phase === "phase-production-build" &&
+      basePath && {
+        basePath: "/duc-huy-hoang-blog-v2",
+        assetPrefix: "/duc-huy-hoang-blog-v2",
+      }),
+  });
+};
+
+export default createConfig;
